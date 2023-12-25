8fa48a536503be16bf835395c9f26bf66aca0abc
Yoseph Berhanu <yoseph.berhanu@aau.edu.et>
2023-12-25 17:27:12 -0600

    Initial Commit

---
+++ src/main/java/et/yoseph/spatialcon/SpatialconApplication.java
@@ -0,0 +1,13 @@
+package et.yoseph.spatialcon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpatialconApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpatialconApplication.class, args);
+	}
+
+}
